73320e42b476591d430cf9fec9433f3b15006539
ha22119 <55187236+ha22119@users.noreply.github.com>
2020-12-03 12:14:52 +0900

    20201203_1

--- bab/app.js
+++ bab/app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var signupRouter = require('./routes/signup');
+var mainRouter = require('./routes/main');
 
 var app = express();
 
@@ -23,6 +24,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/signup', signupRouter);
+app.use('/main', mainRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
